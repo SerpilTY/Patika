9fdb033ef9f4746430499e55acd2fb65f8a35c0b
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-11-30 23:57:16 +0300

    Initial Commit

---
+++ Algorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a word followed by the index of the character you want to extract (Use a comma between them.):");
+        string word = Console.ReadLine();            
+        string[] sections=word.Split(","); 
+        string firstWord=sections[0]; 
+        int number=int.Parse(sections[1]);
+        firstWord=firstWord.Remove(number,1);
+        Console.WriteLine(firstWord);
+
+}}
