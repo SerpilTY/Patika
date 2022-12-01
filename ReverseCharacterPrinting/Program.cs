fea039b2b5a2b214e25d3f087d87c61bf2c80205
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-12-01 23:07:57 +0300

    Initial Commit

---
+++ ReverseCharacterPrinting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ReverseCharacterPrinting;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sentence that I will reverse the characters:");
+        string sentence = Console.ReadLine();
+        string[] sections = sentence.Split(" ");
+        string reverseString = "";
+        string word = "";
+        char[] letters;
+
+        for (int i = 0; i < sections.Length; i++)
+        {
+            word = sections[i];
+            letters = new char[word.Length];
+
+
+            for (int j = 0; j < word.Length; j++)
+            {
+                letters[j] = word[j];
+
+            }
+
+            for (int k = 1; k < letters.Length; k++)
+            {
+                char gecici = letters[k - 1];
+                letters[k - 1] = letters[k];
+                letters[k] = gecici;
+            }
+
+            if (reverseString.Length == 0)
+            {
+                reverseString = new string(letters);
+            }
+            else
+            {
+                reverseString += " " + new string(letters);
+            }
+        }
+
+        Console.WriteLine(reverseString);
+
+    }
+}
+
