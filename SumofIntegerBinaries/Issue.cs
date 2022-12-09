6cff77adea142074f94210bc2e98de0afaa23580
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-12-09 23:36:43 +0300

    Rafactored

--- SumofIntegerBinaries/Issue.cs
+++ SumofIntegerBinaries/Issue.cs
@@ -7,50 +7,29 @@ public class Issue{
    public static ArrayList FinalDizi = new ArrayList();
 
 public static void GetAndCalculateValues(){
-
-        System.Console.WriteLine("Enter n numbers, could be seperate or same. Write down how much number you will enter");
+    System.Console.WriteLine("Enter n numbers, could be seperate or same. Write down how much number you will enter");
         int n = int.Parse(System.Console.ReadLine());
         int[] Numbers = new int[n];
 
         for (int i = 0; i < n; i++)
         {
-            System.Console.WriteLine("Enter a number: ");
+            System.Console.WriteLine("Enter the {0}. number: ",i+1);
             Numbers[i] = int.Parse(Console.ReadLine());
         }
         double sonuc;
-        for (int i = 0; i < 1; i++)
+       
+        for (int i = 0; i < n; i +=2)
         {
             if (Numbers[i] == Numbers[i + 1])
             {
-                sonuc = Math.Pow((Numbers[0] + Numbers[1]), 2);
-                FinalDizi.Add(sonuc);
+                sonuc = Math.Pow((Numbers[i] + Numbers[i + 1]), 2);
+                System.Console.Write(sonuc+" ");
             }
             else if (Numbers[i] != Numbers[i + 1])
             {
-                sonuc = (Numbers[0] + Numbers[1]);
-                FinalDizi.Add(sonuc);
-            }
-        }
-        for (int i = 2; i < n; i++)
-        {
-            if (i % 2 == 0 && Numbers[i] == Numbers[i + 1])
-            {
-                sonuc = Math.Pow((Numbers[i] + Numbers[i + 1]), 2);
-                FinalDizi.Add(sonuc);
-            }
-            else if (i % 2 == 0 && Numbers[i] != Numbers[i + 1])
-            {
                 sonuc = (Numbers[i] + Numbers[i + 1]);
-                FinalDizi.Add(sonuc);
+                System.Console.Write(sonuc+" ");
             }
-            else break;
+            else continue;
         }
-}    
-public static void PrintValues(IEnumerable myList)
-        {
-            Console.WriteLine("*****************");
-            foreach (Object obj in myList)
-            Console.Write("   {0}", obj);
-            Console.WriteLine("\n*****************");
-        }
-}
+}    }
