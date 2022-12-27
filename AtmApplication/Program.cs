4b7ff16372ac66e646d2fc31655540ec06a061af
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-12-27 16:56:16 +0300

    Refactor

--- AtmApplication/Program.cs
+++ AtmApplication/Program.cs
@@ -3,25 +3,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Entities.User user=new Entities.User();
-        user.Id=1;
-        user.CardNumber="54";
-        user.Password="55";
+        Entities.User user = new Entities.User();
+        user.Id = 1;
+        user.CardNumber = "54";
+        user.Password = "55";
         Register.Users.Add(user);
-                
-        if(Register.UserCheck())
-        
+        Logger logger = new Logger();
+
+        if (Register.UserCheck())
+        {
+
             System.Console.WriteLine("Which operation you would like to do?");
             System.Console.WriteLine("Press 1 for Withdrawal, 2 for Payment and 3 for Deposit please.");
             string operation = Console.ReadLine();
-            Business Atm=new Business();
+            Business Atm = new Business();
             if (operation == "1") Atm.Withdrawal();
             else if (operation == "2") Atm.Payment();
             else if (operation == "3") Atm.Deposit();
-            
+        }
 
-        } 
+        logger.EndOfTheDayReadFile();
 
     }
+}
 
- 
